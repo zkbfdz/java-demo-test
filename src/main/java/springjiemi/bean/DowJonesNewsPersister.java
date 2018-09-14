1fee7fa2c92a42064d6fc1dc30ea50c9323e7b9f
zhu kai <1158102632@qq.com>
2018-09-14 18:29:35 +0800

    1

---
+++ src/main/java/springjiemi/bean/DowJonesNewsPersister.java
@@ -0,0 +1,11 @@
+package springjiemi.bean;
+
+/**
+ * Created by kai.zhu on 2018/9/14/014.
+ */
+public class DowJonesNewsPersister implements IFXNewsPersister {
+    @Override
+    public void persistNews(FXNewsBean newsBean) {
+        System.out.println("DowJonesNewsPersister.persistNews");
+    }
+}
